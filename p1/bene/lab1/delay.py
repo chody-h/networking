26b1ae095a36c5283e3a7608890259e6c20e47e8
Cody Heffner <cody.heffner@gmail.com>
2015-01-21 16:36:42 -0700

    Done with data collection, but queueing theory is pretty off.

---
+++ p1/bene/lab1/delay.py
@@ -0,0 +1,74 @@
+import sys
+sys.path.append('../')
+
+from src.sim import Sim
+from src import node
+from src import link
+from src import packet
+
+from networks.network import Network
+
+import random
+import argparse
+
+class Generator(object):
+    def __init__(self,node,destination,load,duration):
+        self.node = node
+        self.load = load
+        self.duration = duration
+        self.start = 0
+        self.ident = 1
+
+    def handle(self,event):
+        # quit if done
+        now = Sim.scheduler.current_time()
+        if (now - self.start) > self.duration:
+            return
+
+        # generate a packet
+        self.ident += 1
+        p = packet.Packet(destination_address=destination,ident=self.ident,protocol='delay',length=1000)
+        Sim.scheduler.add(delay=0, event=p, handler=self.node.send_packet)
+        # schedule the next time we should generate a packet
+        Sim.scheduler.add(delay=random.expovariate(self.load), event='generate', handler=self.handle)
+
+class DelayHandler(object):
+    def receive_packet(self,packet):
+        print Sim.scheduler.current_time(),packet.ident,packet.created,Sim.scheduler.current_time() - packet.created,packet.transmission_delay,packet.propagation_delay,packet.queueing_delay
+
+if __name__ == '__main__':
+
+    #arguments
+    parser = argparse.ArgumentParser(prog='Delayed Packets', description='Helper of Project 1 of CS 460 at BYU.')
+    # valid arguments for -s:
+    # all 2n1 2n2 2n3 3n1a 3n1b 3n2 qt
+    parser.add_argument('-u', '--util', type=float, action='store', default='all')
+    args = parser.parse_args()
+    utilization = args.util
+
+    # parameters
+    Sim.scheduler.reset()
+
+    # setup network
+    net = Network('../networks/one-hop.txt')
+
+    # setup routes
+    n1 = net.get_node('n1')
+    n2 = net.get_node('n2')
+    n1.add_forwarding_entry(address=n2.get_address('n1'),link=n1.links[0])
+    n2.add_forwarding_entry(address=n1.get_address('n2'),link=n2.links[0])
+
+    # setup app
+    d = DelayHandler()
+    net.nodes['n2'].add_protocol(protocol="delay",handler=d)
+
+    # setup packet generator
+    destination = n2.get_address('n1')
+    max_rate = 1000000/(1000*8)
+
+    load = utilization*max_rate
+    g = Generator(node=n1,destination=destination,load=load,duration=10)
+    Sim.scheduler.add(delay=0, event='generate', handler=g.handle)
+    
+    # run the simulation
+    Sim.scheduler.run()
--- p1/bene/lab1/delayparser.py
+++ p1/bene/lab1/delayparser.py
@@ -13,9 +13,26 @@ class Delayparser(object):
 			if (line[0] != 'R' and line[0] != '\n'):
 				avg += float(line.split(' ')[6].split('\n')[0])
 				count += 1
-		return avg / count
+		return [count, avg / count]
 
 if __name__ == '__main__':
 	d = Delayparser()
 
-	print d.parse("outputs/qt_test.txt")
+	util = [10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 98]
+
+	for u in util:
+		f = "outputs/qt_" + str(u) + ".txt"
+		ret = d.parse(f)
+		print "{:.0f}% utilization: {:.0f} packets, {:.6f} average queue delay.".format(u, ret[0], ret[1])
+
+	# print "10% utilization: " + str(d.parse("outputs/qt_10.txt"))
+	# print "20% utilization: " + str(d.parse("outputs/qt_20.txt"))
+	# print "30% utilization: " + str(d.parse("outputs/qt_30.txt"))
+	# print "40% utilization: " + str(d.parse("outputs/qt_40.txt"))
+	# print "50% utilization: " + str(d.parse("outputs/qt_50.txt"))
+	# print "60% utilization: " + str(d.parse("outputs/qt_60.txt"))
+	# print "70% utilization: " + str(d.parse("outputs/qt_70.txt"))
+	# print "80% utilization: " + str(d.parse("outputs/qt_80.txt"))
+	# print "90% utilization: " + str(d.parse("outputs/qt_90.txt"))
+	# print "95% utilization: " + str(d.parse("outputs/qt_95.txt"))
+	# print "98% utilization: " + str(d.parse("outputs/qt_98.txt"))
